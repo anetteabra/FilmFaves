29bd56eeb6cce66ba802cfff2a83828eaba75a34
Frida Benedikte Iversen <fridabi@stud.ntnu.no>
2024-09-18 12:45:53 +0200

    have made the search bar functinal

--- prosjekt1/src/routes/home/index.tsx
+++ prosjekt1/src/routes/home/index.tsx
@@ -1,62 +1,13 @@
 import styles from './Home.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
 import MovieCarousel from '../../components/MovieCarousel';
-import { useSearchParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { fetchMovies } from '../../api/tmdbApi';
-import MovieBox from '../../components/MovieBox';
-
-interface Movie {
-  poster_path: string;
-  title: string;
-}
+import SearchBar from '../../components/SeachBar';
 
 const Home = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [searchQuery, setSearchQuery] = useState(searchParams.get("search"));
-  
-  const movieQuery = useQuery<Movie[]>({
-    queryKey: ['movies'],
-    queryFn: fetchMovies,
-  });
-
-  const isInSearch = (value: string) => {
-    return (value || "")
-      .toLowerCase()
-      .includes(searchParams.get("search")?.toLowerCase()|| "");
-  };
-
-  useEffect(() => {
-    setSearchParams({
-      ...(searchQuery && { search: searchQuery }),
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchQuery]);
-
 
   return (
     <>
       <div className={styles.controls}>
-        <div className={styles.searchContainer}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-          <input type="text" className={styles.search} 
-          value={searchParams.get("search") || ""}
-          onChange={(event) =>  {
-            setSearchQuery(event.target.value);
-          }}
-          />
-        </div>
-        <div id="setList" className={styles.setList}>
-          {movieQuery.status === "success" && 
-            movieQuery.data
-              .filter(
-                (set) =>
-                  isInSearch(set.title)
-              )
-              .map((set) => <MovieBox posterPath={set.poster_path} title={set.title} />)}
-        </div>
+        <SearchBar/> 
         <MovieCarousel />
       </div>
     </>
