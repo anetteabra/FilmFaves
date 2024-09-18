747b2f3ddb1b26caec3537e0318aaaf99f8bc444
liselun <liselun@ntnu.no>
2024-09-18 20:23:59 +0200

    feat: sorting by rating
    add sorting functionality for rating
    
    issue: #17

--- prosjekt1/src/routes/home/index.tsx
+++ prosjekt1/src/routes/home/index.tsx
@@ -1,15 +1,27 @@
+import { useState, useEffect } from 'react';
 import styles from './Home.module.css';
-import MovieCarousel from '../../components/MovieCarousel';
 import SearchBar from '../../components/SeachBar';
+import SortingBox from '../../components/SortingBox';
+import MovieCarousel from '../../components/MovieCarousel';
 
 const Home = () => {
+  const defaultOption = 'carousel';
+  const [sortOption, setSortOption] = useState<string>(defaultOption);
+
+  useEffect(() => {
+    const savedOption = sessionStorage.getItem('sortOption');
+    if (savedOption) {
+      setSortOption(savedOption);
+    }
+  }, []);
 
   return (
     <>
       <div className={styles.controls}>
-        <SearchBar/> 
-        <MovieCarousel />
+        <SearchBar />
+        <SortingBox onSortChange={setSortOption} />
       </div>
+      <MovieCarousel sortOption={sortOption} />
     </>
   );
 };
