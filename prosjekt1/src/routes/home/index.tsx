d50a0b2fd5dd87d4d9ddfd18cd5030fbcf41f582
Mari Stampeløkken <marstamp@stud.ntnu.no>
2024-09-19 13:21:41 +0200

    feat: implemented snapshot testing.
    Implemented snapshot test for home page, have commented out the searchbar since it is not functional atm, to be able to check that the snapshot works.
    
    issue #19

--- prosjekt1/src/routes/home/index.tsx
+++ prosjekt1/src/routes/home/index.tsx
@@ -1,13 +1,13 @@
 import styles from './Home.module.css';
 import MovieCarousel from '../../components/MovieCarousel';
-import SearchBar from '../../components/SeachBar';
+// import SearchBar from '../../components/SeachBar';
 
 const Home = () => {
 
   return (
     <>
       <div className={styles.controls}>
-        <SearchBar/> 
+        {/* <SearchBar/>  */}
         <MovieCarousel />
       </div>
     </>
