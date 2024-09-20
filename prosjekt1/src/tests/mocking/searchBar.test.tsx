ba8ca66e3c9984d20df0d4344036e7aa166dd268
Mari Stampeløkken <marstamp@stud.ntnu.no>
2024-09-20 13:56:47 +0200

    fix: removed extra code
    unnecessary code removed

--- prosjekt1/src/tests/mocking/searchBar.test.tsx
+++ prosjekt1/src/tests/mocking/searchBar.test.tsx
@@ -1,14 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
 import SearchBar from '../../components/SeachBar';
-import axios from 'axios';
 import { MemoryRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
-
-// Mock axios
-vi.mock('axios');
 
 describe('SearchBar Component', () => {
     it('should render the search bar', () => {
@@ -19,7 +15,6 @@ describe('SearchBar Component', () => {
             </MemoryRouter>
           </QueryClientProvider>
         );
-        screen.debug();
 
         const inputElement = screen.getByTestId('search-input');
         expect(inputElement).toBeInTheDocument();
